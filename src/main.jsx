cc8e33a8c817547b7adff04c0ad3fbb24e254d8c
Omkar <omkarapte2016@gmail.com>
2026-05-24 20:16:15 -0400

    fix: main repetition fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css' // <-- Add this exact line to inject your styles!
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
